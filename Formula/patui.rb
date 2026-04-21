8587d6473048009d0ae2aa4f32c3709e83b54e26
George Mandis <george@mand.is>
2026-04-21 16:48:57 -0400

    fix: standalone binary with native sharp bindings, no bun dep

--- Formula/patui.rb
+++ Formula/patui.rb
@@ -7,34 +7,32 @@ class Patui < Formula
   on_macos do
     on_arm do
       url "https://github.com/georgemandis/patui/releases/download/v0.1.0/patui-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "8eaf4d71864531b2f1007f54b23e2cd2decd07bc0dbb7fb7b0153bf5c5c6db67"
+      sha256 "8453709f9d46732fbabffe1acea767b4c8a2a68cc37e52fd21115c7f5625e7cb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/georgemandis/patui/releases/download/v0.1.0/patui-v0.1.0-linux-aarch64.tar.gz"
-      sha256 "72bd57d687bd7d3343316f51e09658dff85d9822d9c4c94effa6334ce432333e"
+      sha256 "544bf178e6921bc34db80b2593299bce44cf472630b197b41797e12dc80566f7"
     else
       url "https://github.com/georgemandis/patui/releases/download/v0.1.0/patui-v0.1.0-linux-x86_64.tar.gz"
-      sha256 "9a466c859e41226a01db90341198093de1028ab5eebad6c2b8e758e3b045f845"
+      sha256 "2279b412734f3d523388bb22c19cfbdda4837c2fc85f9569fb95d3d2befcb577"
     end
   end
 
   def install
-    bin.install "patui"
-    libexec.install "sharp"
+    libexec.install "patui"
+    libexec.install "node_modules"
 
-    # Wrapper so the binary can find sharp's native bindings
-    mv bin/"patui", libexec/"patui"
     (bin/"patui").write <<~SH
       #!/usr/bin/env bash
-      NODE_PATH="#{libexec}/sharp" exec "#{libexec}/patui" "$@"
+      cd "#{libexec}" && exec ./patui "$@"
     SH
     chmod 0755, bin/"patui"
   end
 
   test do
-    assert_match "patui", (bin/"patui").read
+    assert_predicate bin/"patui", :exist?
   end
 end
