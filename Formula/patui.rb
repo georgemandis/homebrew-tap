d8e1b0206f7fa36c4fbb9138982393d69f5afd3b
George Mandis <george@mand.is>
2026-04-22 18:27:58 -0400

    bump patui to v0.2.1

--- Formula/patui.rb
+++ Formula/patui.rb
@@ -1,23 +1,23 @@
 class Patui < Formula
   desc "Terminal-based image editor with Vim-style modal controls"
   homepage "https://github.com/georgemandis/patui"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/georgemandis/patui/releases/download/v0.2.0/patui-v0.2.0-macos-aarch64.tar.gz"
-      sha256 "136173eaa89147f9c54214d88b87ee129883c5ed23a0f0dcfdc73d1a9a52860a"
+      url "https://github.com/georgemandis/patui/releases/download/v0.2.1/patui-v0.2.1-macos-aarch64.tar.gz"
+      sha256 "dd7cde4bb7f826272eb191f650797db9400a55d9ebedd4783fead057ec55ca3a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/patui/releases/download/v0.2.0/patui-v0.2.0-linux-aarch64.tar.gz"
-      sha256 "ec86aa08e8845470675bba6e06c71b085bd2036d34efd1cc920ef3ea2718f321"
+      url "https://github.com/georgemandis/patui/releases/download/v0.2.1/patui-v0.2.1-linux-aarch64.tar.gz"
+      sha256 "f9c27607b07686ee161410bd3ff461847bc23abb34b997dbf1833bf51f24ed70"
     else
-      url "https://github.com/georgemandis/patui/releases/download/v0.2.0/patui-v0.2.0-linux-x86_64.tar.gz"
-      sha256 "c6352e5616248ec925efa4b2483d053350a4077aec30676969717cc0eaaa44af"
+      url "https://github.com/georgemandis/patui/releases/download/v0.2.1/patui-v0.2.1-linux-x86_64.tar.gz"
+      sha256 "7fce847e5e6920e02bdc39c236c69bed51e1aa7c288eaf30dd8ae3f0bd5e2f72"
     end
   end
 
