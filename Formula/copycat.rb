86d95d56b13236ee79e89b0f52f0b59950b656f8
George Mandis <george@mand.is>
2026-05-21 14:36:37 -0400

    Update copycat to v0.3.0 (Zig 0.16.0)

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -1,26 +1,26 @@
 class Copycat < Formula
   desc "Cross-platform clipboard CLI and C ABI library written in Zig"
   homepage "https://github.com/georgemandis/copycat"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.2.0/copycat-v0.2.0-macos-aarch64.tar.gz"
-      sha256 "a8edb08a0e29f6ca0a117c9a327a7fe5bc9ace670601a193b90aaf83b4c4b05c"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-macos-aarch64.tar.gz"
+      sha256 "5e7eb9f22e2c432f4cad42b42559a2d6a74881fb68cee8e4e26181e82d17e6d8"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.2.0/copycat-v0.2.0-macos-x86_64.tar.gz"
-      sha256 "c0e1d5e68aa90c11c914a77843302ba2c4e9618d2d329163cf74a3780be2363c"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-macos-x86_64.tar.gz"
+      sha256 "28fbc7658d547e89236abdaaf885154e65832b32a4aed0362994db8e9dbe0a2c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.2.0/copycat-v0.2.0-linux-aarch64.tar.gz"
-      sha256 "2a3029a04686655b19dca2b9b0f3659469a10a34fdfefe5daebe5d802d40d441"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-linux-aarch64.tar.gz"
+      sha256 "b65223b57e2a9b5fe1cbfdd859c9556341c768426ebe41430c6f9f36ac4bdebe"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.2.0/copycat-v0.2.0-linux-x86_64.tar.gz"
-      sha256 "181af4adb52b2531da0b0d5225bf26fd91d667ff75028e873615ee427aca8c8b"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-linux-x86_64.tar.gz"
+      sha256 "5852fc82abf1de3b09d0400d470015ee8603cc0b4f1860ee484dc540f818762e"
     end
   end
 
