47e0353e75f58e26e89f9baf18374d174ee4d63e
George Mandis <george@mand.is>
2026-05-21 13:17:51 -0400

    whereami: update to v0.3.0 (Zig 0.16)

--- Formula/whereami.rb
+++ Formula/whereami.rb
@@ -1,26 +1,26 @@
 class Whereami < Formula
-  desc "Get your current location from the command line using native OS APIs"
+  desc "Get your current location from the command-line using native OS APIs"
   homepage "https://github.com/georgemandis/whereami"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/whereami/releases/download/v0.2.1/whereami-v0.2.1-macos-aarch64.tar.gz"
-      sha256 "459c0621bb05f793cb645248e9ddc422b24154d444b6b9814cf82a5602fb0b32"
+      url "https://github.com/georgemandis/whereami/releases/download/v0.3.0/whereami-v0.3.0-macos-aarch64.tar.gz"
+      sha256 "6286b70497d69d64e196e1b1e3c630c343ad409265448cf68868c9ac15516335"
     else
-      url "https://github.com/georgemandis/whereami/releases/download/v0.2.1/whereami-v0.2.1-macos-x86_64.tar.gz"
-      sha256 "739c6aff6f5593a5b53ce01cbf9b7dfdb2eed40f6e3540770367c03b6394d089"
+      url "https://github.com/georgemandis/whereami/releases/download/v0.3.0/whereami-v0.3.0-macos-x86_64.tar.gz"
+      sha256 "481fe06a72fef3e00b77d29c9ed242bea5111327df2e3392ac43dea220d5b9e6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/whereami/releases/download/v0.2.1/whereami-v0.2.1-linux-aarch64.tar.gz"
-      sha256 "62720b5f620722f5b79823ec2aeeb06b03093b142b338f4e043f57293fd85f8f"
+      url "https://github.com/georgemandis/whereami/releases/download/v0.3.0/whereami-v0.3.0-linux-aarch64.tar.gz"
+      sha256 "761b41a091dc4a48f5358317555adebf19a035971d60390fdc211111a266e300"
     else
-      url "https://github.com/georgemandis/whereami/releases/download/v0.2.1/whereami-v0.2.1-linux-x86_64.tar.gz"
-      sha256 "c92f2e25a0833d75653fad8679056af0d7521522beef476efa10b1054fe45321"
+      url "https://github.com/georgemandis/whereami/releases/download/v0.3.0/whereami-v0.3.0-linux-x86_64.tar.gz"
+      sha256 "1e8d1fad1173dfade89faf82b5d2a2e5677da4765c5d9e1d3c9aa75a643840ad"
     end
   end
 
