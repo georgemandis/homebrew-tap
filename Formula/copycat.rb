b0e1cf44b6fdd32ebb79afa44eef4a34a83a2dbd
George Mandis <george@mand.is>
2026-05-21 15:21:53 -0400

    Update copycat to v0.3.1

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -1,26 +1,26 @@
 class Copycat < Formula
   desc "Cross-platform clipboard CLI and C ABI library written in Zig"
   homepage "https://github.com/georgemandis/copycat"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-macos-aarch64.tar.gz"
-      sha256 "5e7eb9f22e2c432f4cad42b42559a2d6a74881fb68cee8e4e26181e82d17e6d8"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.1/copycat-v0.3.1-macos-aarch64.tar.gz"
+      sha256 "2d66977a36d3e9d27063506ab1eff0b31c78c2c358eb3dae098a82c6a0b42b21"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-macos-x86_64.tar.gz"
-      sha256 "28fbc7658d547e89236abdaaf885154e65832b32a4aed0362994db8e9dbe0a2c"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.1/copycat-v0.3.1-macos-x86_64.tar.gz"
+      sha256 "b6b047f580d7145f7a1c36cb943fb82252ab6319ab7d0cfa3131932a35d54f29"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-linux-aarch64.tar.gz"
-      sha256 "b65223b57e2a9b5fe1cbfdd859c9556341c768426ebe41430c6f9f36ac4bdebe"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.1/copycat-v0.3.1-linux-aarch64.tar.gz"
+      sha256 "5c02c5889a27db9a9b43908db168ebdd58489053af89aa833745cc409df4e70d"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.3.0/copycat-v0.3.0-linux-x86_64.tar.gz"
-      sha256 "5852fc82abf1de3b09d0400d470015ee8603cc0b4f1860ee484dc540f818762e"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.3.1/copycat-v0.3.1-linux-x86_64.tar.gz"
+      sha256 "d7f1c16acc1e41b08ef4de697a42d5b6804091f503960aa37c6c027a6ef82bc3"
     end
   end
 
