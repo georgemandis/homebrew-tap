d4df2fde63ef2a23c5b9a48cf51ce40686bf1b50
George Mandis <george@mand.is>
2026-05-22 00:58:59 -0400

    nearme: fill in release SHAs for v0.1.0

--- Formula/nearme.rb
+++ Formula/nearme.rb
@@ -7,10 +7,10 @@ class Nearme < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/georgemandis/nearme/releases/download/v0.1.0/nearme-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "6e6e62540fffb008c9e894898e916b0553d6eace80930e7a47d929a78cf17db9"
     else
       url "https://github.com/georgemandis/nearme/releases/download/v0.1.0/nearme-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "56241b72e3923dc32ffe8a66ba37e2f69f448cb3e9c1386de6c0e201cce2db73"
     end
   end
 
