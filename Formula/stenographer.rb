26adf600e431f9214bf7ffe1ead42be4459b20b5
George Mandis <george@mand.is>
2026-05-29 15:36:08 -0400

    Update stenographer to v0.2.1, tezcatl to v0.1.1
    Both now include shell completions (bash, zsh, fish) auto-installed by brew.

--- Formula/stenographer.rb
+++ Formula/stenographer.rb
@@ -1,20 +1,23 @@
 class Stenographer < Formula
   desc "Speech-to-text from the command line using native macOS Speech Recognition"
   homepage "https://github.com/georgemandis/stenographer"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.0/stenographer-v0.2.0-macos-aarch64.tar.gz"
-      sha256 "eff64487fccde6bfd1d823af74e50de7471d466b9a7f6265072e9ee97ab11ab9"
+      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.1/stenographer-v0.2.1-macos-aarch64.tar.gz"
+      sha256 "3f4b75f6ec7bc9b57abee9deb4bc56f2f9bdd97e2b36964a6942112e3446b987"
     else
-      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.0/stenographer-v0.2.0-macos-x86_64.tar.gz"
-      sha256 "2a5cbd6f97c06332f455d9efb55df461ceb00c4c4269536c1c2d4f8e8be59457"
+      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.1/stenographer-v0.2.1-macos-x86_64.tar.gz"
+      sha256 "5411f3be285453a8b42e59807f0d35c4e613e3bb6995042359bee7a17d28d142"
     end
   end
 
   def install
     bin.install "stenographer"
+    bash_completion.install "completions/stenographer.bash" => "stenographer"
+    zsh_completion.install "completions/stenographer.zsh" => "_stenographer"
+    fish_completion.install "completions/stenographer.fish"
   end
 
   test do
--- Formula/tezcatl.rb
+++ Formula/tezcatl.rb
@@ -1,20 +1,23 @@
 class Tezcatl < Formula
   desc "Lightweight CLI for rendering web pages using native macOS WebKit"
   homepage "https://github.com/georgemandis/tezcatl"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.0/tezcatl-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "ff3f87c871da72a10fb36925f5d88e9741fd8eb29bd32a9c5c47164360409e25"
+      url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.1/tezcatl-v0.1.1-macos-aarch64.tar.gz"
+      sha256 "c5662988bf0b920a5fbb546179841d9bd992d59e9bdc845f30ebc335a1910fcc"
     else
-      url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.0/tezcatl-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "b529139cf59e9eb9dead262fc33519a1a5174be7a8a258c6eefd3bb54927268c"
+      url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.1/tezcatl-v0.1.1-macos-x86_64.tar.gz"
+      sha256 "34296fe6c921788b776584119bdab8b21861e7df517e67f5f185c7f7aff1242e"
     end
   end
 
   def install
     bin.install "tezcatl"
+    bash_completion.install "completions/tezcatl.bash" => "tezcatl"
+    zsh_completion.install "completions/tezcatl.zsh" => "_tezcatl"
+    fish_completion.install "completions/tezcatl.fish"
   end
 
   test do
