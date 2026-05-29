c50557fd5eaacf648b49389b9df0be967f52104c
George Mandis <george@mand.is>
2026-05-29 00:21:26 -0400

    Update tezcatl v0.1.0 SHA256 hashes
    Rebuilt release to fix misleading example in help text

--- Formula/tezcatl.rb
+++ Formula/tezcatl.rb
@@ -6,10 +6,10 @@ class Tezcatl < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.0/tezcatl-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "a28b0f92b512a9972d8ade2b047403ba907313c94be3a4adbdd2ae5c1f7bac12"
+      sha256 "ff3f87c871da72a10fb36925f5d88e9741fd8eb29bd32a9c5c47164360409e25"
     else
       url "https://github.com/georgemandis/tezcatl/releases/download/v0.1.0/tezcatl-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "c7241caa91ced895c9074acb99f115ffc91073140614d19488a4ab92c167d2ca"
+      sha256 "b529139cf59e9eb9dead262fc33519a1a5174be7a8a258c6eefd3bb54927268c"
     end
   end
 
