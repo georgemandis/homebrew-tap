23274b757748eba57a09c5dc6c471ec1cbfddd98
George Mandis <george@mand.is>
2026-05-29 15:19:20 -0400

    Update stenographer to v0.2.0

--- Formula/stenographer.rb
+++ Formula/stenographer.rb
@@ -1,15 +1,15 @@
 class Stenographer < Formula
   desc "Speech-to-text from the command line using native macOS Speech Recognition"
   homepage "https://github.com/georgemandis/stenographer"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/stenographer/releases/download/v0.1.0/stenographer-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "b4db83eb6db5c37a91657f95aafd71dc79375aa2dd5008101a1d3da9ba147b8b"
+      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.0/stenographer-v0.2.0-macos-aarch64.tar.gz"
+      sha256 "eff64487fccde6bfd1d823af74e50de7471d466b9a7f6265072e9ee97ab11ab9"
     else
-      url "https://github.com/georgemandis/stenographer/releases/download/v0.1.0/stenographer-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "623a0cf1591157a1343ead2a242f6c1a8eeddf85392d370d52429b137f54fae5"
+      url "https://github.com/georgemandis/stenographer/releases/download/v0.2.0/stenographer-v0.2.0-macos-x86_64.tar.gz"
+      sha256 "2a5cbd6f97c06332f455d9efb55df461ceb00c4c4269536c1c2d4f8e8be59457"
     end
   end
 
