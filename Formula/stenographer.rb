d3b3de00179fd3517a32fc2280eeec1d2a55ed77
George Mandis <george@mand.is>
2026-05-28 20:24:28 -0400

    Update SHA256 hashes for stenographer v0.1.0

--- Formula/stenographer.rb
+++ Formula/stenographer.rb
@@ -6,10 +6,10 @@ class Stenographer < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/georgemandis/stenographer/releases/download/v0.1.0/stenographer-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "b4db83eb6db5c37a91657f95aafd71dc79375aa2dd5008101a1d3da9ba147b8b"
     else
       url "https://github.com/georgemandis/stenographer/releases/download/v0.1.0/stenographer-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "623a0cf1591157a1343ead2a242f6c1a8eeddf85392d370d52429b137f54fae5"
     end
   end
 
