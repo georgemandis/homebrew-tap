0002ad1e681700dd05b4e7417df110097960e856
George Mandis <george@mand.is>
2026-05-29 23:41:43 -0400

    copycat: bump to 0.4.2
    Builds with ReleaseSafe to eliminate debug allocator leak reports.

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -1,26 +1,26 @@
 class Copycat < Formula
   desc "Cross-platform clipboard CLI and C ABI library written in Zig"
   homepage "https://github.com/georgemandis/copycat"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.1/copycat-v0.4.1-macos-aarch64.tar.gz"
-      sha256 "754c2d19fc1bbeb50deb082c52cc563b61a8a7220caa6736d076eee07ac23363"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.2/copycat-v0.4.2-macos-aarch64.tar.gz"
+      sha256 "f485007004a43fd2f0ec814a39e56dc763c3c6c9c8580ef347b082eb426c8567"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.1/copycat-v0.4.1-macos-x86_64.tar.gz"
-      sha256 "405ce619d7ead24e9e1d780117884c165839b1dd620299338ec63d78a26ebced"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.2/copycat-v0.4.2-macos-x86_64.tar.gz"
+      sha256 "3c3d2c00285b39116070a8a97274b0b74fd284a739b370bc1e20fb3f54fc2f00"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.1/copycat-v0.4.1-linux-aarch64.tar.gz"
-      sha256 "59b7994ba81510864b667027fc11106b7af21a847d924bdba7ebb255325b08d9"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.2/copycat-v0.4.2-linux-aarch64.tar.gz"
+      sha256 "5c6970982abc631457814de8509dbab002c946e486217917b6db60de35c6f77e"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.1/copycat-v0.4.1-linux-x86_64.tar.gz"
-      sha256 "8bb88009cf480f6b012e8528abaad183bb8fc9fbb8db1de64ed91cd571753e30"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.2/copycat-v0.4.2-linux-x86_64.tar.gz"
+      sha256 "e033178983c387ff5431f1ad46dd80bab74c755cbe5553baf8aa746c7e7684b3"
     end
   end
 
