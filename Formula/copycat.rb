3d8e273ac2a3b90e1e1f85903bf42401fcef46da
George Mandis <george@mand.is>
2026-05-30 01:15:45 -0400

    copycat: bump to 0.4.4

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -1,26 +1,26 @@
 class Copycat < Formula
   desc "Cross-platform clipboard CLI and C ABI library written in Zig"
   homepage "https://github.com/georgemandis/copycat"
-  version "0.4.3"
+  version "0.4.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.3/copycat-v0.4.3-macos-aarch64.tar.gz"
-      sha256 "05353b4bab5c452a7d38d4a13f9db837a117b2a152cbc49523b91ae1b107db47"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.4/copycat-v0.4.4-macos-aarch64.tar.gz"
+      sha256 "d9031f8bd5b5b09a670508e1506a592cf8f2a1abfc53cfef9d17a77f53cca368"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.3/copycat-v0.4.3-macos-x86_64.tar.gz"
-      sha256 "4dee25060d528699af6370f9160c92068fbbd968266deeb7c89d0980e3d8d614"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.4/copycat-v0.4.4-macos-x86_64.tar.gz"
+      sha256 "eb32f71ff573ea05b8caa8a54c74e904d2d4ba5fdb8d77fb9fbcc2483e24fdcf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.3/copycat-v0.4.3-linux-aarch64.tar.gz"
-      sha256 "c2a3f897ebc0358581bb069240ab8277a371da4763276f7536229e8db49af9af"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.4/copycat-v0.4.4-linux-aarch64.tar.gz"
+      sha256 "ebf54e612031fdc0885d09e5b52357afbcc5f7b09c6149eef7b389d77cdc656c"
     else
-      url "https://github.com/georgemandis/copycat/releases/download/v0.4.3/copycat-v0.4.3-linux-x86_64.tar.gz"
-      sha256 "8c2ccddc62281f0b3ecc5f4a4692372dde4802fd89337eb7e915519075245a8d"
+      url "https://github.com/georgemandis/copycat/releases/download/v0.4.4/copycat-v0.4.4-linux-x86_64.tar.gz"
+      sha256 "a999fb78fb454951eedfa104d5faaae50c36702deb559a7b0a7d2ae841b6cbc6"
     end
   end
 
