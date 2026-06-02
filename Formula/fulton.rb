73a4a6c48534e2e6190f982782b505d4e0b70ffb
George Mandis <george@mand.is>
2026-06-02 16:17:56 -0400

    fulton: Update to v0.2.0 with correct hashes

--- Formula/fulton.rb
+++ Formula/fulton.rb
@@ -1,26 +1,26 @@
 class Fulton < Formula
   desc "Cross-platform global keyboard shortcut daemon and C ABI library written in Zig"
   homepage "https://github.com/georgemandis/fulton"
-  version "0.1.1"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/fulton/releases/download/v0.1.1/fulton-v0.1.1-macos-aarch64.tar.gz"
-      sha256 "f8c1eb004af6d6bf5c0d191f41082500e8d1e89314042f9a115a400c613273e6"
+      url "https://github.com/georgemandis/fulton/releases/download/v0.2.0/fulton-v0.2.0-macos-aarch64.tar.gz"
+      sha256 "365f2d5eee2dcc49b38ecb859640c541de7546259eb17dc711fbd31b4e4cb641"
     else
-      url "https://github.com/georgemandis/fulton/releases/download/v0.1.1/fulton-v0.1.1-macos-x86_64.tar.gz"
-      sha256 "2681eef64407a8bb1aee2c5868f6980b749ad4f83aa6aeaac324bd383a963a62"
+      url "https://github.com/georgemandis/fulton/releases/download/v0.2.0/fulton-v0.2.0-macos-x86_64.tar.gz"
+      sha256 "3af4221a19f81c76091ac0fa157172a23ac8481c1a049de51f5dde01c8770bb2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/fulton/releases/download/v0.1.1/fulton-v0.1.1-linux-aarch64.tar.gz"
-      sha256 "4bd3905e926f4fef4b3c5c64e5f7b5af5895ae05c0ae5c775561a49eab6a2311"
+      url "https://github.com/georgemandis/fulton/releases/download/v0.2.0/fulton-v0.2.0-linux-aarch64.tar.gz"
+      sha256 "98f0e661ffe38443506289d54f26d459359e11e6e5490b6f83d32cd2f3ff8098"
     else
-      url "https://github.com/georgemandis/fulton/releases/download/v0.1.1/fulton-v0.1.1-linux-x86_64.tar.gz"
-      sha256 "75a418dd425b565035d70036a456ddfd9451728eac61e5da88aae443c03c820d"
+      url "https://github.com/georgemandis/fulton/releases/download/v0.2.0/fulton-v0.2.0-linux-x86_64.tar.gz"
+      sha256 "4b4334e529632c7f7cda0a604824515a92509fc64e504b2e894e55ac181563c1"
     end
   end
 
