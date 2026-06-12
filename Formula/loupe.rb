ad9e2340a6fd2e3413d1927740a69b74bb143a61
George Mandis <george@mand.is>
2026-06-12 19:34:01 -0400

    loupe 0.4.0

--- Formula/loupe.rb
+++ Formula/loupe.rb
@@ -1,16 +1,16 @@
 class Loupe < Formula
   desc "Computer vision CLI for face detection, OCR, and barcode scanning"
   homepage "https://github.com/georgemandis/loupe"
-  version "0.3.1"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/loupe/releases/download/v0.3.1/loupe-v0.3.1-macos-aarch64.tar.gz"
-      sha256 "548f4971ad83d12bdd1605f916484b8f73b4712a8b3d027f2bd6a26c10c0cfa8"
+      url "https://github.com/georgemandis/loupe/releases/download/v0.4.0/loupe-v0.4.0-macos-aarch64.tar.gz"
+      sha256 "d3308eb8042072c5c1251b0bf9c529b1c612f19cd7e6107125e91747cdf3a616"
     else
-      url "https://github.com/georgemandis/loupe/releases/download/v0.3.1/loupe-v0.3.1-macos-x86_64.tar.gz"
-      sha256 "1993f0cee118940102cb29aef9d0bdf64c723f797e31d87436194bba57a59770"
+      url "https://github.com/georgemandis/loupe/releases/download/v0.4.0/loupe-v0.4.0-macos-x86_64.tar.gz"
+      sha256 "4bdf23202f2bf9272a9483fdabbcff3dc9d6c6c2e8a4b92ac64acdf6ae477292"
     end
   end
 
