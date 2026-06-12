4c9bbb006b9516309f0f17263f5d6d067dfda512
George Mandis <george@mand.is>
2026-06-12 19:25:21 -0400

    Update spectacle to v0.2.0
    Adds screenshot command for single-frame capture to PNG/JPEG.

--- Formula/spectacle.rb
+++ Formula/spectacle.rb
@@ -1,16 +1,16 @@
 class Spectacle < Formula
   desc "Screen and system audio capture CLI using native OS APIs"
   homepage "https://github.com/georgemandis/spectacle"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/georgemandis/spectacle/releases/download/v0.1.0/spectacle-v0.1.0-macos-aarch64.tar.gz"
-      sha256 "7deba1e0aec671f8bad373a1584ea7f250c37ffb8c029126261de58c143f32f0"
+      url "https://github.com/georgemandis/spectacle/releases/download/v0.2.0/spectacle-v0.2.0-macos-aarch64.tar.gz"
+      sha256 "794dd040f94aef559a30506b82e9b8ff9710c4dca43fb0c54e3bac5940c0b22b"
     else
-      url "https://github.com/georgemandis/spectacle/releases/download/v0.1.0/spectacle-v0.1.0-macos-x86_64.tar.gz"
-      sha256 "37f5117378b053a305cb8914fd165985f378c895503f4ce5d211a59d7c457168"
+      url "https://github.com/georgemandis/spectacle/releases/download/v0.2.0/spectacle-v0.2.0-macos-x86_64.tar.gz"
+      sha256 "59ddacbfe8803e445c94f9b068df8d4218c7f6e8f6d161f246b47f67528db5c0"
     end
   end
 
